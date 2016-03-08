13bfc97a9798be95bdefa9de98e166d57c95fd2c
bberges10 <bberges10@gmail.com>
2016-03-08 12:47:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,8 +26,7 @@ function respond() {
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("Sebastian    8-9853	
-Steve	8-5449		
+    postMessage("Steve	8-5449		
 Erick	8-2416		
 Elliot	8-3606		
 David	8-3624		
