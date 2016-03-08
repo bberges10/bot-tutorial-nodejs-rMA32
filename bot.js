b45300d78bfd1f7e74c95b866ccf1a2ac291eae9
bberges10 <bberges10@gmail.com>
2016-03-08 13:02:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,23 +26,7 @@ function respond() {
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("	Sebastian    8-9853	
-		Steve	8-5449
-		Erick	8-2416
-		Eliot	8-3606
-		Facilities	8-8282
-		UM Operator	8-2211
-		Raiser's Edge	8-1349	
-		May Peralta	8-5938	
-		Student IT 	8-8887	
-		Blackboard	8-3949
-		Events - Maria Otero	8-2362	
-		Alex	786-747-5704
-		Sebastian  305-989-9286
-		Steve	305-606-2971
-		Eliot	786-303-3463
-		Erick	786-405-5782
-");
+    postMessage(" Sebastian- 8-9853 | Steve- 8-5449 | Erick 8-2416 | Eliot 8-3606 | May Peralta 8-5938 | Events (Maria Otero) 8-2362 | Blackboard 8-3949 |");
     this.res.end();
   } 
   else if(request.text && botRegexAd.test(request.text)) {
